51d38b070da85607f4bf7a0e6ace77644fd41a7b
cuong tran <you@example.com>
2024-09-22 10:52:05 +0700

    view

---
+++ src/main/java/com/example/view/model/Counter.java
@@ -0,0 +1,25 @@
+package com.example.view.model;
+
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void increment() {
+        count++;
+    }
+}
